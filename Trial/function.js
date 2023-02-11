03d29ca531585e2f18309e818a4da0be93b92294
farid.isfiyanto@gmail.com <farid.isfiyanto@gmail.com>
2023-02-11 15:49:51 +0700

    Adding jawaban soal2

---
+++ Trial/function.js
@@ -0,0 +1,31 @@
+// // Fungsi jenis 1
+// function namaFungsi1(){
+//     console.log("Hello World! 1");
+//  }
+
+// // Fungsi jenis 2 
+// var namaFungsi2 = function(){
+//     console.log("Hello World! 2");
+//  }
+
+// // Fungsi jenis 3
+// var namaFungsi3 = () => {
+//     console.log("Hello World! 3");
+//  }
+
+// // atau seperti ini (jika isi fungsi hanya satu baris):
+// var namaFungsi4 = () => console.log("Hello World! 4");
+
+// var namaFungsi5 = new Function('console.log("Hello World! 5");');
+
+// function fungsiBagi(a,b){
+//     let c = a/b
+//     console.log("Hasil bagi a & b adalah " + c)
+// }
+
+// fungsiBagi(4,2)
+// namaFungsi1()
+// namaFungsi2()
+// namaFungsi3()
+// namaFungsi4()
+// namaFungsi5()
